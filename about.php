fafdb401f301c53bfe0a053bfa34a908cf39b937
Syviiiiic <125569515+Syviiiiic@users.noreply.github.com>
2025-07-25 21:48:33 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php include 'includes/header.php'; ?>
+
+<main>
+    <section class="about">
+        <h1>О нас</h1>
+        <p>Мы - команда профессионалов, создающая качественные веб-решения.</p>
+        <p>Наш опыт более 10 лет в области веб-разработки.</p>
+    </section>
+</main>
+
+<?php include 'includes/footer.php'; ?>
