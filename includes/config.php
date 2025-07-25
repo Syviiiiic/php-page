d57f0c7423dfa8017e25a9a15dbe96d339e18f01
Syviiiiic <125569515+Syviiiiic@users.noreply.github.com>
2025-07-25 21:50:36 +0300

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+// Базовые настройки
+define('SITE_NAME', 'Мой Лендинг');
+define('SITE_URL', 'http://yourdomain.com');
+define('ADMIN_EMAIL', 'admin@yourdomain.com');
+
+// Настройки отображения ошибок
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
